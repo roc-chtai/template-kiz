35b4924f36d2dc21ddf62548c00792ecc96a5cdc
roc-chtai <roc.chtai@gmail.com>
2025-06-12 15:12:03 +0800

    Update kidz.js

--- assets/js/kidz.js
+++ assets/js/kidz.js
@@ -606,11 +606,8 @@
 });
 
 $('.scrollup').click(function(){
-  // 滾動到 .top-bar 元素的位置
-  $('html, body').animate({
-    scrollTop: $('.bg-danger.top-bar').offset().top
-  }, 500);  // 設定動畫時間為 500 毫秒
-  return false;  // 防止默認行為
+  $('html, body').animate({ scrollTop: 0 }, 500);
+  return false;
 });
 
 
